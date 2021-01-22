698cf1dbe76e9a67c6db69e5405268917b1362e6
Janna123-it <akmatova60583@mail.ru>
2021-01-22 13:51:25 +0600

    copied index.html

--- urls.py
+++ urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import  test, third
+from main.views import  test, third, static
+
 
 
 
