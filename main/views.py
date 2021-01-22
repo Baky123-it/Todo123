d99b61b6a4fc0117dfb0cef27ba5f16c7f0e8e40
Janna123-it <akmatova60583@mail.ru>
2021-01-22 14:14:55 +0600

    first commit

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
     
+    def homepage (request):
+        return render(request, "index.html")
+        
 def test (request):
     return render (request,"test.html")
 
